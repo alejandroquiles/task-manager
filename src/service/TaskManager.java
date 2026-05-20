2de3bad5ab2f0312f4c115f94641208c3aa46303
Alejandro Quiles Rodríguez <alejandro.quiles.ro@gmail.com>
2026-05-20 18:42:28 +0200

    feat: permitir consultar tareas del gestor

--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -3,12 +3,15 @@ import java.util.ArrayList;
 import model.Task;
 
 public class TaskManager {
-	private ArrayList <Task> tasks;
+	private ArrayList<Task> tasks;
 	public TaskManager () {
 		this.tasks = new ArrayList<>();
 	}
 	public void addTask(String title) {
 		Task task = new Task(title);
 		this.tasks.add(task);
+	}
+	public ArrayList<Task> getTasks() {
+		return this.tasks;
 	}
 }
