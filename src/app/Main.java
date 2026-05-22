05430660a9369d8cedfe778d35afa749f78827c4
Alejandro Quiles Rodríguez <alejandro.quiles.ro@gmail.com>
2026-05-22 19:03:20 +0200

    feat: añadir menú interactivo básico

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,18 +1,48 @@
 package app;
+import java.util.Scanner;
 import model.Task;
 import service.TaskManager;
+
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		TaskManager taskManager = new TaskManager();
-		taskManager.addTask("Estudiar Git");
-		taskManager.addTask("Practicar Java");
+		Scanner scanner = new Scanner(System.in);
+		boolean running = true;
+		while(running){
 		
-		for (Task task : taskManager.getTasks()) {
-		    System.out.println(task.getTitle());
+				System.out.println("==== TASK MANAGER ====");
+				System.out.println();
+				System.out.println("1. Añadir tarea");
+				System.out.println("2. Listar tareas");
+				System.out.println("3. Salir");
+				System.out.println();
+				System.out.print("Elige una opción: ");
+
+				String option = scanner.nextLine();
+				switch (option) {
+				case "1":
+					System.out.println("Has elegido añadir tarea");
+					break;
+				case "2":
+					System.out.println("Has elegido listar tareas");
+					break;
+				case "3":
+					running = false;
+					System.out.println("Saliendo del programa...");
+					break;
+				default:
+					System.out.println("Opción no válida");
+					break;
+				}
+				
+			}
+			
+		}
+		
 		}
 
-	}
 
-}
+
