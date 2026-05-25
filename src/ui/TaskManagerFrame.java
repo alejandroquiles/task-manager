a9709cf9fdf5a2feb45b39870bba0d26bb181082
Alejandro Quiles Rodríguez <alejandro.quiles.ro@gmail.com>
2026-05-25 19:53:22 +0200

    feat: permitir completar tareas desde la interfaz Swing

--- src/ui/TaskManagerFrame.java
+++ src/ui/TaskManagerFrame.java
@@ -12,6 +12,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.JOptionPane;
+import model.Task;
 
 import service.TaskManager;
 
@@ -19,6 +21,7 @@ public class TaskManagerFrame extends JFrame {
 
 	private JTextField taskInput;
 	private JButton addButton;
+	private JButton completeButton;
 	private TaskManager taskManager;
 	private DefaultListModel<String> taskListModel;
 	private JList<String> taskList;
@@ -40,10 +43,12 @@ public class TaskManagerFrame extends JFrame {
 		JLabel titleLabel = new JLabel("Task Manager", SwingConstants.CENTER);
 		titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
 
+		JPanel bottomPanel = new JPanel(new BorderLayout());
 		JPanel inputPanel = new JPanel();
 
 		this.taskInput = new JTextField(20);
 		this.addButton = new JButton("Añadir tarea");
+		this.completeButton = new JButton("Marcar como completada");
 
 		this.taskListModel = new DefaultListModel<>();
 		this.taskList = new JList<>(this.taskListModel);
@@ -61,12 +66,33 @@ public class TaskManagerFrame extends JFrame {
 				System.out.println("Tarea añadida: " + title);
 			}
 		});
+		
+		this.completeButton.addActionListener(e -> {
+			int selectedIndex = this.taskList.getSelectedIndex();
 
+			if (selectedIndex == -1) {
+				JOptionPane.showMessageDialog(this, "Selecciona una tarea primero.");
+				return;
+			}
+
+			boolean completed = this.taskManager.completeTask(selectedIndex);
+
+			if (completed) {
+				Task task = this.taskManager.getTasks().get(selectedIndex);
+				this.taskListModel.setElementAt("[Completada] " + task.getTitle(), selectedIndex);
+			} else {
+				JOptionPane.showMessageDialog(this, "No se pudo completar la tarea.");
+			}
+		});
+		
 		inputPanel.add(this.taskInput);
 		inputPanel.add(this.addButton);
 
+		bottomPanel.add(inputPanel, BorderLayout.NORTH);
+		bottomPanel.add(this.completeButton, BorderLayout.SOUTH);
+
 		this.add(titleLabel, BorderLayout.NORTH);
 		this.add(scrollPane, BorderLayout.CENTER);
-		this.add(inputPanel, BorderLayout.SOUTH);
+		this.add(bottomPanel, BorderLayout.SOUTH);
 	}
 }
