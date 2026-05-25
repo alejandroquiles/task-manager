f013483a7a13e944779069296ce64da244e416e7
Alejandro Quiles Rodríguez <alejandro.quiles.ro@gmail.com>
2026-05-25 19:26:10 +0200

    feat: añadir controles iniciales a la interfaz Swing

--- src/ui/TaskManagerFrame.java
+++ src/ui/TaskManagerFrame.java
@@ -1,13 +1,44 @@
 package ui;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Font;
 
-public class TaskManagerFrame extends JFrame{
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class TaskManagerFrame extends JFrame {
+
+	private JTextField taskInput;
+	private JButton addButton;
+
 	public TaskManagerFrame() {
 		this.setTitle("Task Manager");
-		this.setSize(500,400);
+		this.setSize(500, 400);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
+
+		this.initComponents();
 	}
 
+	private void initComponents() {
+		this.setLayout(new BorderLayout());
+
+		JLabel titleLabel = new JLabel("Task Manager", SwingConstants.CENTER);
+		titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+
+		JPanel inputPanel = new JPanel();
+
+		this.taskInput = new JTextField(20);
+		this.addButton = new JButton("Añadir tarea");
+
+		inputPanel.add(this.taskInput);
+		inputPanel.add(this.addButton);
+
+		this.add(titleLabel, BorderLayout.NORTH);
+		this.add(inputPanel, BorderLayout.CENTER);
+	}
 }
